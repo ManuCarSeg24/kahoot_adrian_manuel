acd456b77d910e8dc4f867fa14777a83d75b17ec
ManuCarSeg24 <manucarseg@gmail.com>
2025-12-11 09:25:19 +0100

    Creación de las vistas

---
+++ app/src/main/java/com/example/kahoot_adrian_manuel/Juego.kt
@@ -0,0 +1,17 @@
+package com.example.kahoot_adrian_manuel
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class Juego : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.layout_juego, container, false)
+    }
+}
--- app/src/main/java/com/example/kahoot_adrian_manuel/MainActivity.kt
+++ app/src/main/java/com/example/kahoot_adrian_manuel/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.kahoot_adrian_manuel
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var layoutConfigurarPreguntas: LinearLayout
+    private lateinit var layoutJuego: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,25 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    // Cargamos el menú
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menu_configuracion -> {
+                layoutConfigurarPreguntas.visibility = View.VISIBLE
+                layoutJuego.visibility = View.GONE
+            }
+            R.id.menu_jugar -> {
+                layoutConfigurarPreguntas.visibility = View.GONE
+                layoutJuego.visibility = View.VISIBLE
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
