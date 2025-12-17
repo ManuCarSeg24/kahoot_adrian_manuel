8b9b8543c446b1c55d8a05b0b936f600925d16b9
ManuCarSeg24 <manucarseg@gmail.com>
2025-12-17 12:37:59 +0100

    Cmabios de menu, utilizamos Toolbar

--- app/src/main/java/com/example/kahoot_adrian_manuel/MainActivity.kt
+++ app/src/main/java/com/example/kahoot_adrian_manuel/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +20,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        toolbar.overflowIcon?.setTint(
+            resources.getColor(android.R.color.white, theme)
+        )
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
